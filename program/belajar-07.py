9ec13c4d188dee00de863005e5e13a663e4f2561
alfath <alfath.khadijah@gmail.com>
2015-09-13 19:07:01 +0700

    Belajar While

---
+++ program/belajar-07.py
@@ -0,0 +1,8 @@
+__author__ = 'alfath'
+print 'Program ini akan membuat komputer teruuuus berpikir selama-lamanya'
+
+ingin_pintar = True
+
+while ingin_pintar:
+    print 'belajar'
+    ingin_pintar = False
